619dc66c900220a27d6dc8f5f3fde3ab2f85ecd3
Yue <cathywang428@gmail.com>
2021-12-01 14:25:35 -0600

    updated functions for the game

--- connect4.js
+++ connect4.js
@@ -8,7 +8,7 @@
 const WIDTH = 7;
 const HEIGHT = 6;
 
-let currPlayer = 1; // active player: 1 or 2
+let currPlayer = 'RED'; // active player: 1 or 2
 const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
@@ -59,19 +59,32 @@ function makeHtmlBoard() {
 
 function findSpotForCol(x) {
 	// TODO: write the real version of this, rather than always returning 0
-	return 0;
+	for (let y = 5; y >= 0; y--) {
+		let cell = document.getElementById(`${y}-${x}`);
+		if (cell.children.length === 0) {
+			return y;
+		}
+	}
+	return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
 	// TODO: make a div and insert into correct table cell
+	const circleDiv = document.createElement('div');
+	const cell = document.getElementById(`${y}-${x}`);
+	circleDiv.classList.add('piece');
+	circleDiv.classList.add(currPlayer);
+	cell.appendChild(circleDiv);
+	board[y][x] = currPlayer;
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
 	// TODO: pop up alert message
+	alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -97,9 +110,32 @@ function handleClick(evt) {
 
 	// check for tie
 	// TODO: check if all cells in board are filled; if so call, call endGame
+	if (checkEndGame()) {
+		return endGame('Game Over! Tied!');
+	}
 
 	// switch players
 	// TODO: switch currPlayer 1 <-> 2
+	currPlayer = switchPlayer(currPlayer);
+}
+
+//switch currPlayer 1 <-> 2
+function switchPlayer(player) {
+	return player === 'RED' ? 'BLUE' : 'RED';
+}
+
+//check if all cells in board are filled, if so, return true
+function checkEndGame() {
+	for (let y = 0; y < HEIGHT; y++) {
+		for (let x = 0; x < WIDTH; x++) {
+			let cell = document.getElementById(`${y}-${x}`);
+			if (cell.children.length === 0) {
+				//console.dir(cell);
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
